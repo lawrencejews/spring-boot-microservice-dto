2590ed60c1d8d26d221f57f93c40e692f62a7365
lawrencejews <lawrencejews1@gmail.com>
2024-03-20 17:55:41 +0900

    webClient for microservice-communication

--- README.md
+++ README.md
@@ -1,6 +1,10 @@
 #### SpringBoot-Microservices
 
-##### Rest Template - microservice communication
+#### Microservices Communication
+##### Rest Template
 - Mapping communication between services
 - Create a field in the JPA Entity and a class DTO.
 - Configure RestTemplate as Spring Bean, then Inject and use the RestTemplate to make REST API calls.
+##### WebClient
+- Add Spring webFlux dependency, configure WebClient as spring Bean
+- Inject and use WebClient to call the REST API and test with Postman.
--- employee-service/src/main/java/com/lawrencejews/employeeservice/service/impl/EmployeeServiceImpl.java
+++ employee-service/src/main/java/com/lawrencejews/employeeservice/service/impl/EmployeeServiceImpl.java
@@ -10,6 +10,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Service
 @AllArgsConstructor
@@ -17,7 +18,9 @@ public class EmployeeServiceImpl implements EmployeeService {
 
     private EmployeeRepository employeeRepository;
 
-    private RestTemplate restTemplate;
+    private WebClient webClient;
+
+//    private RestTemplate restTemplate;
 
     @Override
     public EmployeeDto saveEmployee(EmployeeDto employeeDto) {
@@ -46,12 +49,17 @@ public class EmployeeServiceImpl implements EmployeeService {
     public APIResponseDto getEmployeeById(Long employeeId) {
 
     Employee employee = employeeRepository.findById(employeeId).get();
-    ResponseEntity<DepartmentDto> responseEntity = restTemplate.getForEntity(
-                "http://localhost:8080/api/departments/" + employee.getDepartmentCode(),
-                DepartmentDto.class);
-    DepartmentDto departmentDto = responseEntity.getBody();
 
+//    ResponseEntity<DepartmentDto> responseEntity = restTemplate.getForEntity(
+//                "http://localhost:8080/api/departments/" + employee.getDepartmentCode(),
+//                DepartmentDto.class);
+//    DepartmentDto departmentDto = responseEntity.getBody();
 
+    DepartmentDto departmentDto = webClient.get()
+            .uri("http://localhost:8080/api/departments/" + employee.getDepartmentCode())
+            .retrieve()
+            .bodyToMono(DepartmentDto.class)
+            .block();
 
     EmployeeDto employeeDto = new EmployeeDto(
       employee.getId(),
